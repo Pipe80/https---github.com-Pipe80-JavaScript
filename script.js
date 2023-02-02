bb6aee9c4d172d0a67b5c0bc9fbf07b5b2ff84ad
Pirjo Mure <N5589@student.jamk.fi>
2023-02-02 09:21:51 +0200

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest=true;
+
+if (passTest) hasDriversLicense =true;{
+    if (hasDriversLicense)
+    console.log('Drivers license is valid');
+}
+
+// const interface='Audio';
+// const private=534;
+function logger() {
+console.log('My name is Pirjo');
+}
+//calling the function
+logger();
