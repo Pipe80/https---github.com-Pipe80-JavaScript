b719f252cf9f19fb7198b502a288a8595bf6dbab
Pirjo Mure <N5589@student.jamk.fi>
2023-02-02 09:38:28 +0200

    update

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ console.log('My name is Pirjo');
 }
 //calling the function
 logger();
+
+function fruitProcessor(apples, oranges) {
+// console.log(apples, oranges);
+const juice =`Juice with ${apples} apples and ${oranges} oranges`;
+return juice;
+}
+const appleJuice =fruitProcessor(5,0); //arvo on muuttujassa
+console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice=fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
