5db866294b277441032bda89abc434d69b07433e
Pirjo Mure <N5589@student.jamk.fi>
2023-02-02 10:35:38 +0200

    update

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ console.log(appleJuice);
 const appleOrangeJuice=fruitProcessor(2,4);
 console.log(appleOrangeJuice);
 
+const num=Number('23')
+
+//function declaration
+function calcAge1(birthYear) {
+return 2023-birthYear;
+}
+
+const age1=calcAge1(1980);
+console.log(age1);
+
+//function expression
+const calcAge2= function (birthYear) {
+    return 2023-birthYear;}
+
+const age2=calcAge2(1936);
+console.log(age2);
+
+//arrow function
+const calcAge3= birthYear => 2023-birthYear;
+const age3 = calcAge3(1950);
+console.log(age3);
+
+const yearsUntilRetirement=(birthYear, firstName) => {
+    const age=2023-birthYear;
+    const retirement=65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+
+
+}
+console.log(yearsUntilRetirement(1980,'Pipe'));
+console.log(yearsUntilRetirement(1990,'Polla'));
