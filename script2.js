f6e5fb9a80ea8b33cfad89e851b45c51191551af
Pipe80 <pirjomure@hotmail.com>
2023-02-09 12:05:24 +0200

    update

--- script2.js
+++ script2.js
@@ -1,1 +1,27 @@
 
+const pipe={
+    firstName: 'Pipe',
+    lastName:'Coder',
+    age: 2023-1980,
+    job: 'student',
+    friends: ['imaginery', 'none']
+}
+
+console.log(pipe.firstName);
+console.log(pipe['lastName']);
+
+const nameKey = 'Name';
+console.log(pipe['first'+nameKey]);
+console.log(pipe['last'+nameKey]);
+
+const interestedIn= prompt('What do you want to know about Pipe? Choose between firstName, lastName, age, job and friends');
+
+if (pipe[interestedIn]) {
+console.log(pipe[interestedIn]);
+} else{
+    console.log('wrong, Choose between firstName, lastName, age, job and friends');
+}
+
+pipe.location='Laukaa';
+pipe ['linkedin']='@somethingLinkedin';
+console.log(pipe);
